f94f1f8300510b10eebf5c65c6e30d1dc97dbcfc
JEON-DONG-HEE <bethetopone@gmail.com>
2021-08-08 19:58:53 +0900

    210808_02 화살표 함수 기초1

--- App.js
+++ App.js
@@ -1,2 +1,4 @@
-const test = "test";
-console.log(test);
+const userItem = ["바나나", "방울토마토", "오이"];
+const userItemBest = userItem.map(userItem => userItem + " 쵝오야!!!"); // 화살표 함수의 사용
+
+console.log(userItemBest);
